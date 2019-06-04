66b4f5a98bc77e487745b312ee8749621d37ec7e
Ludvig Londos <ludvig.londos@gmail.com>
2019-06-04 14:52:02 +0200

    first version

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  '098-1282823',
+    category:      'japanese'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number:  '080-8080808',
+    category:      'italian'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
