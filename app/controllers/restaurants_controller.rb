551cfe01e32166d07380fddd6980c2932de4af8a
Ludvig Londos <ludvig.londos@gmail.com>
2019-06-04 15:18:06 +0200

    done with rake

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -10,12 +10,16 @@ class RestaurantsController < ApplicationController
 
   def new
     @restaurant = Restaurant.new
+
   end
 
   def create
     @restaurant = Restaurant.new(restaurant_params)
-    @restaurant.save
-    redirect_to restaurants_path
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
   private
